ca5966ee71a1b4964bd0e8de85f0b58bd2419a13
Thugette <didi_toleva@mail.bg>
2016-01-29 20:23:13 +0200

    Product class, Category and Condition Enumerations

---
+++ app/src/main/java/model/Category.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by owner on 29/01/2016.
+ */
+public enum Category {
+    CLOTHES, ELECTRONIC, FURNITURE, SPORT, MUSIC, BOOK, ANIMALS, SHOES, COSMETICS, ACCESSORIES
+}
