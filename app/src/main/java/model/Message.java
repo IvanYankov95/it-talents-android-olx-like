8c1b0d9f5ca2764e9fce108bc99f2103788a7edb
Ivan <ivanyankov95@yahoo.com>
2016-01-29 15:55:18 +0200

    Created UserAcc and Message

---
+++ app/src/main/java/model/Message.java
@@ -0,0 +1,31 @@
+package model;
+
+import android.support.annotation.NonNull;
+
+import java.util.Date;
+
+/**
+ * Created by Ivan on 29-Jan-16.
+ */
+public class Message implements Comparable<Message> {
+
+    private String heading;
+    private String text;
+    private Date date;
+
+    public Message(String heading, String text) {
+        this.heading = heading;
+        this.text = text;
+        this.date = new Date();
+    }
+
+    @Override
+    public int compareTo(@NonNull Message another) {
+        if(date.after(another.date))
+            return 1;
+        if(date.before(another.date))
+            return -1;
+
+        return 0;
+    }
+}
---
+++ app/src/main/java/model/UserAcc.java
@@ -0,0 +1,136 @@
+package model;
+
+import android.util.Patterns;
+import java.util.TreeSet;
+
+/**
+ * Created by Ivan on 29-Jan-16.
+ */
+public class UserAcc {
+
+    CharSequence email;//this field is required
+    private String password;//this field is required
+
+    private String userName;//this field is required
+    private String firstName;//this field is required
+    private String lastName;//this field is required
+
+    private String phoneNumber; //this field is required
+    private String secondPhoneNumber; //optional
+
+    private String address;//optional?
+
+    private TreeSet<Message> messages = new TreeSet<>();
+
+    public UserAcc(String mail, String password, String userName) throws IllegalArgumentException {
+        this.setEmail(mail);
+        this.setPassword(password);
+        this.setUserName(userName);
+    }
+
+    public void sendMessage(UserAcc userAcc, String heading, String text){
+        Message message = new Message(heading, text);
+
+        userAcc.receiveMessage(message);
+    }
+
+    public void receiveMessage(Message message){
+        this.messages.add(message);
+    }
+
+    public void deleteUserAcc(){
+        //TODO delete from database
+    }
+
+    public CharSequence getEmail() {
+        return email;
+    }
+
+    public void setEmail (CharSequence email) throws IllegalArgumentException{
+        if(Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            //TODO check in databese for existing email.
+            this.email = email;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    //това ще трябва да се промени само засега е така. Ще трябва да проверяваме дали паролата огтоваря на изискванята но след това да я криптираме и записваме.
+    public void setPassword(String password) throws IllegalArgumentException {
+        char[] test = password.toCharArray();
+
+        boolean lowercase = false;
+        boolean uppercase = false;
+        boolean number = false;
+
+        if(test.length < 8)
+            throw new IllegalArgumentException();
+
+        for (char aTest : test) {
+            if (aTest >= 65 && aTest <= 90)
+                lowercase = true;
+            if (aTest >= 97 && aTest <= 122)
+                uppercase = true;
+            if (aTest >= 48 && aTest <= 57)
+                number = true;
+        }
+
+        if(!lowercase || !uppercase || !number)
+            throw new IllegalArgumentException();
+
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        //TODO check in database
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSecondPhoneNumber() {
+        return secondPhoneNumber;
+    }
+
+    public void setSecondPhoneNumber(String secondPhoneNumber) {
+        this.secondPhoneNumber = secondPhoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
