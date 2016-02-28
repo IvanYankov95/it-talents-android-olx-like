d6c4e079c2862ddb6de050e67c6200549b7b0a13
Mitakaa <dlazarov94@gmail.com>
2016-02-28 16:51:45 +0200

    SendMessage activity added

--- app/src/main/java/bg/ittalents/olxlike/SendMessage.java
+++ app/src/main/java/bg/ittalents/olxlike/SendMessage.java
@@ -3,11 +3,11 @@ package bg.ittalents.olxlike;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ViewUserActivity extends AppCompatActivity {
+public class SendMessage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_user);
+        setContentView(R.layout.activity_send_message);
     }
 }
