6092ece5d37d93b57214c8794e62d0fdfebcf3e8
Mitakaa <dlazarov94@gmail.com>
2016-02-28 16:27:13 +0200

    ViewUser activity added

---
+++ app/src/main/java/bg/ittalents/olxlike/ViewUserActivity.java
@@ -0,0 +1,13 @@
+package bg.ittalents.olxlike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_user);
+    }
+}
