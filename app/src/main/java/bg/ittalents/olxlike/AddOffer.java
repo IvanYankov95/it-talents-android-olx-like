5072b351b3c7aae4525861b633fbb558220b6b68
Mitakaa <dlazarov94@gmail.com>
2016-02-28 20:02:25 +0200

    AddOffer activity

---
+++ app/src/main/java/bg/ittalents/olxlike/AddOffer.java
@@ -0,0 +1,13 @@
+package bg.ittalents.olxlike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddOffer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_offer);
+    }
+}
