f2d9babbe03729206b81a2adeb01bf326aeeac6f
Mitakaa <dlazarov94@gmail.com>
2016-02-29 19:01:33 +0200

    AddOffer activity

---
+++ app/src/main/java/bg/ittalents/olxlike/MyMessages.java
@@ -0,0 +1,13 @@
+package bg.ittalents.olxlike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyMessages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_messages);
+    }
+}
