bc9a2d9c541c5620e52e6dd9e558ca249470e9ae
Mitakaa <dlazarov94@gmail.com>
2016-03-01 16:16:59 +0200

    search and category

---
+++ app/src/main/java/bg/ittalents/olxlike/Category.java
@@ -0,0 +1,13 @@
+package bg.ittalents.olxlike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
